d30f19b21173879bc00f3955803156bbbe763948
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-08-12 19:03:07 +1000

    Init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('password validation', () => {
+	describe('on blank form', () => {
+		it('has a password field', () => {
+			render(<App />)
+
+			const passwordField = screen.getByLabelText('Password')
+
+			expect(passwordField).toHaveValue('')
+		})
+
+		it('has a password confirmation field', () => {
+			render(<App />)
+
+			const confirmPasswordField = screen.getByLabelText('Confirm password')
+
+			expect(confirmPasswordField).toHaveValue('')
+		})
+	})
+})
