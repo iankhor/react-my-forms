ef966a3c0897b912e521d39e3bc52b5dacf6c7d1
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-08-13 23:14:43 +1000

    SPIKE reset server response on key change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import React, { ChangeEvent, useReducer } from 'react'
+import React, { useMemo, useState, ChangeEvent, useReducer, useEffect } from 'react'
 import './App.css'
+
+type Form = {
+  password: string | null
+}
 
 type FormState = {
   password: string | null
@@ -19,11 +23,15 @@ type Action =
       property: keyof FormState
       value: any
     }
+  | {
+      type: 'submit'
+      errors: any
+    }
 
 const initFormState = {
   password: null,
   errors: {
-    password: false,
+    password: [],
   },
 }
 
@@ -33,9 +41,12 @@ function reducer(state: FormState, action: Action) {
       return { ...state, [action.property]: action.value.trim() }
     case 'blur': {
       const newState = { ...state, [action.property]: action.value.trim() }
+      return { ...newState, errors: { ...validateFields(newState) } }
+    }
+    case 'submit': {
       return {
-        ...newState,
-        errors: { ...validateFields(newState) },
+        ...state,
+        errors: { ...action.errors },
       }
     }
   }
@@ -43,12 +54,17 @@ function reducer(state: FormState, action: Action) {
 
 type ErrorKeys = 'blank' | 'length'
 
+const generateError = () => {
+  const errors = ['invalid', 'someErrorKey', '']
+  return [errors[Math.floor(Math.random() * errors.length)]].filter(Boolean)
+}
+
 function isBlank(value: string | null): ErrorKeys {
-  return (value === '' ? 'blank' : '') as ErrorKeys
+  return (value === '' || value === null ? 'blank' : '') as ErrorKeys
 }
 
 function isLength(value: string | null, length: number): ErrorKeys {
-  return (typeof value === 'string' && value.length < length ? 'length' : '') as ErrorKeys
+  return ((typeof value === 'string' && value.length < length) || value === null ? 'length' : '') as ErrorKeys
 }
 
 function validateFields(state: FormState): any {
@@ -59,42 +75,74 @@ function validateFields(state: FormState): any {
 
 function errorMessage(key: ErrorKeys) {
   const errorMessage = {
-    blank: 'cannot be blank',
-    length: 'is too short',
-    default: 'field has errors',
+    blank: 'CLIENT VALIDATION cannot be blank',
+    length: 'CLIENT VALIDATION is too short',
+    invalid: 'SERVER VALIDATION this is invalid',
+    someErrorKey: 'SERVERVALIDATION this is some error key message',
+    default: 'CLIENT VALIDATION DEFAULT field has errors',
   }
 
   return errorMessage[key] || errorMessage.default
 }
 
+function isValid(state: FormState): any {
+  const validatedForm = validateFields(state)
+  return Object.values(validatedForm).flat().length === 0
+}
+
+const useSimulatedSubmitServer = () => {
+  const [isReturnResponse, setIsReturnResponse] = useState<boolean>(false)
+  const error = useMemo(() => generateError(), [isReturnResponse])
+
+  const reset = () => setIsReturnResponse(false)
+
+  const submitForm = (form: Form) => setIsReturnResponse(true)
+
+  return { submitForm, reset, response: isReturnResponse ? { password: error } : null }
+}
+
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initFormState)
+  const { submitForm, response, reset } = useSimulatedSubmitServer()
+
+  const submit = () => {
+    const { password } = state
+    reset()
+    isValid(state) && submitForm({ password })
+  }
 
   return (
     <div className="App">
-      <form>
-        <label>
-          Password
-          <input
-            value={state.password || ''}
-            required
-            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              dispatch({ type: 'change', property: 'password', value: e.target.value })
-            }}
-            onBlur={(e: ChangeEvent<HTMLInputElement>) => {
-              dispatch({ type: 'blur', property: 'password', value: e.target.value })
-            }}
-          />
-        </label>
-        <div role="alert" style={{ display: state.errors.password.length ? '' : 'none' }}>
-          {errorMessage(state.errors.password[0])}
-        </div>
+      <label>
+        Password
+        <input
+          value={state.password || ''}
+          required
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            dispatch({ type: 'change', property: 'password', value: e.target.value })
+          }}
+          onBlur={(e: ChangeEvent<HTMLInputElement>) => {
+            reset()
+            dispatch({ type: 'blur', property: 'password', value: e.target.value })
+          }}
+        />
+      </label>
+      <div role="alert" style={{ display: state.errors.password.length || response?.password.length ? '' : 'none' }}>
+        {errorMessage(state.errors.password[0] || response?.password[0])}
+      </div>
 
-        <label>
-          Confirm password
-          <input value="" onChange={() => {}} />
-        </label>
-      </form>
+      <p>
+        <button disabled={!isValid(state)} onClick={submit}>
+          Submit
+        </button>
+      </p>
+
+      <p>
+        <button onClick={() => reset()}>Reset</button>
+      </p>
+      <hr />
+      {!!response && <pre>Server error responses: {JSON.stringify(response)}</pre>}
+      {<pre>form errors: {JSON.stringify(state.errors)}</pre>}
     </div>
   )
 }
